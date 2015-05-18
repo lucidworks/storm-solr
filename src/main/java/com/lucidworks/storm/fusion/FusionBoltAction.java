bd43db5e999aa61b55bdcaa86b26871a25d561d3
Tim Potter <timpotter@Lucids-MacBook-Pro.local>
2015-05-18 10:10:53 -0600

    LWSHADOOP-269: Allow sending the realm as a parameter on an update request

--- src/main/java/com/lucidworks/storm/fusion/FusionBoltAction.java
+++ src/main/java/com/lucidworks/storm/fusion/FusionBoltAction.java
@@ -15,22 +15,17 @@ import com.lucidworks.storm.solr.JsonContentStreamMapper;
 import com.lucidworks.storm.solr.SolrUpdateRequestStrategy;
 import com.lucidworks.storm.spring.SpringBolt;
 import com.lucidworks.storm.spring.StreamingDataAction;
-import org.apache.http.*;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.AuthState;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpCoreContext;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.impl.LBHttpSolrClient;
 import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
-import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.util.ContentStream;
 
 import org.apache.solr.client.solrj.impl.NoOpResponseParser;
@@ -42,36 +37,12 @@ public class FusionBoltAction implements StreamingDataAction {
   // for basic auth to the pipeline service
   private final class PreEmptiveBasicAuthenticator implements HttpRequestInterceptor {
     private final UsernamePasswordCredentials credentials;
-    private final String realm;
 
-    public PreEmptiveBasicAuthenticator(String user, String pass, String realm) {
+    public PreEmptiveBasicAuthenticator(String user, String pass) {
       this.credentials = new UsernamePasswordCredentials(user, pass);
-      this.realm = realm;
     }
 
     public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
-      if (realm != null) {
-        //request.getParams().setParameter("realm", realm);
-        /*
-        AuthState authState = (AuthState) context.getAttribute(HttpClientContext.TARGET_AUTH_STATE);
-        if (authState.getAuthScheme() == null) {
-          CredentialsProvider credsProvider = new BasicCredentialsProvider();
-          HttpHost targetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
-          AuthScope authScope = new AuthScope(targetHost.getHostName(), targetHost.getPort(), realm);
-          credsProvider.setCredentials(authScope, credentials);
-          context.setAttribute(HttpClientContext.CREDS_PROVIDER, credsProvider);
-          authState.update(new BasicScheme(), credentials);
-          context.setAttribute(HttpClientContext.TARGET_AUTH_STATE, authState);
-        }
-        BasicScheme scheme = new BasicScheme() {
-          @Override
-          public String getRealm() {
-            return realm;
-          }
-        };
-        request.addHeader(scheme.authenticate(credentials, request, context));
-        */
-      }
       request.addHeader((new BasicScheme()).authenticate(credentials, request, context));
     }
   }
@@ -80,6 +51,7 @@ public class FusionBoltAction implements StreamingDataAction {
   protected SolrUpdateRequestStrategy updateRequestStrategy;
   protected JsonContentStreamMapper jsonContentStreamMapper;
   protected String updatePath;
+  protected String fusionRealm;
 
   public FusionBoltAction(String endpoints, String fusionUser, String fusionPass) throws MalformedURLException {
     this(endpoints, fusionUser, fusionPass, null);
@@ -87,6 +59,7 @@ public class FusionBoltAction implements StreamingDataAction {
 
   public FusionBoltAction(String endpoints, String fusionUser, String fusionPass, String fusionRealm) throws MalformedURLException {
     lbHttpSolrClient = new LBHttpSolrClient(endpoints.split(","));
+    this.fusionRealm = fusionRealm;
     initAuthentication(fusionUser, fusionPass, fusionRealm);
   }
 
@@ -96,9 +69,8 @@ public class FusionBoltAction implements StreamingDataAction {
       if (!(httpClient instanceof DefaultHttpClient))
         throw new IllegalStateException("Expected DefaultHttpClient but got "+
           (httpClient.getClass().getName())+" instead!");
-
       DefaultHttpClient dhc = (DefaultHttpClient)httpClient;
-      dhc.addRequestInterceptor(new PreEmptiveBasicAuthenticator(fusionUser, fusionPass, fusionRealm));
+      dhc.addRequestInterceptor(new PreEmptiveBasicAuthenticator(fusionUser, fusionPass));
     }
   }
 
@@ -107,7 +79,8 @@ public class FusionBoltAction implements StreamingDataAction {
     Map<String,Object> values = (Map<String,Object>)input.getValue(1);
     ContentStreamUpdateRequest req = buildUpdateRequest(docId, values);
     req.setResponseParser(new NoOpResponseParser());
-    req.setParam("realm", "foo");
+    if (fusionRealm != null)
+      req.setParam("realm", fusionRealm);
     updateRequestStrategy.sendUpdateRequest(lbHttpSolrClient, updatePath, req);
     return SpringBolt.ExecuteResult.ACK;
   }
